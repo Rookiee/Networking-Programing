deff9daced40f4280d4f6fd43af552c280e41d54
Rookiee <haoyang.xie@gmail.com>
2016-04-04 16:09:16 +0800

    update

--- SMTP/SMTP.py
+++ SMTP/SMTP.py
@@ -1,16 +1,33 @@
-import smtplib
-#from  email.miee.text import MIMEText
-#msg = MIMEText("this is  a test message", "plain", "utf-8")
 
-fromMail = '82154915@qq.com'
+
+import smtplib
+
+from email.header import Header 
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+content = '''
+We are sorry for your computer is being affected by our testing programe.
+You have reinstall your computer to restore the normal email system, 
+otherwise this test email will send by 'yourself' forever.
+Sorry for that.
+
+Please reinstall your OS right now and unplugin your Internet connectiong.
+GB Teams
+'''
+
+msg = MIMEText(content, 'plain', 'utf-8')
+msg['Subject'] = Header("Testing Program Email",'utf-8').encode()
+
+fromMail = 'gbteams@outlook.com'
 toMail = '82154915@qq.com'
 
 server = smtplib.SMTP()
-server.connect('smtp.qq.com',587)
+server.connect('smtp-mail.outlook.com',587)
 server.starttls()
-server.login('82154915@qq.com', 'Xie.Haoyang86052')
-print "login success!"
+server.login('gbteams@outlook.com', 'Tyler_XCCFO')
+print "Login Success"
 
-server.sendmail(fromMail, toMail, "123, this is the second")
-
+# server.sendmail(fromMail, toMail, msg.as_string())
+server.sendmail(fromMail, toMail, content)
 server.quit()
